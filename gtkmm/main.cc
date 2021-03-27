96aea6edf6a6495716e48cc36be8235fa88b8db4
Koki Fukuda <ko.fu.dev@gmail.com>
2021-03-27 18:16:32 +0900

    gtkmm sample

---
+++ gtkmm/main.cc
@@ -0,0 +1,45 @@
+#include <gtkmm.h>
+
+class MainWindow : public Gtk::Window {
+    Gtk::Box container_;
+
+    Gtk::Label label_;
+    Gtk::Button button_;
+    Gtk::Button move_button_;
+
+public:
+    MainWindow()
+        : label_("Hello, world!"), button_("Click me!"),
+          move_button_("Move this window") {
+        set_title("Sample App");
+        set_default_size(400, 300);
+
+        set_gravity(Gdk::GRAVITY_NORTH);
+
+        container_.set_orientation(Gtk::Orientation::ORIENTATION_VERTICAL);
+        container_.set_margin_start(10);
+        container_.set_margin_end(10);
+        container_.add(label_);
+        container_.add(button_);
+        container_.add(move_button_);
+
+        button_.signal_clicked().connect([this] { this->close(); });
+
+        move_button_.signal_clicked().connect([this] {
+            int x;
+            int y;
+            this->get_position(x, y);
+            this->move(x, y + 10);
+        });
+
+        add(container_);
+        show_all_children();
+    }
+};
+
+auto main(int argc, char **argv) -> int {
+    auto app = Gtk::Application::create("org.kofuk.s.gtkmm.Sample");
+
+    MainWindow window;
+    return app->run(window, argc, argv);
+}
