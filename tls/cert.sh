bdb6b6e751e4d640abaf811a190a421889e7174f
Koki Fukuda <ko.fu.dev@gmail.com>
2022-01-16 17:50:03 +0900

    Manipulate cert.sh

--- tls/cert.sh
+++ tls/cert.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 openssl req -x509 -nodes \
-        -subj "/C=JP" \
-        -addext "subjectAltName = DNS:localhost" \
+        -subj "/C=US" \
+        -addext "subjectAltName = DNS:*" \
         -newkey rsa:4096 -keyout server.key -out server.crt
