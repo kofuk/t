f744aca0c4af59f0ccf2988ce1cc7e218d70f19c
Koki Fukuda <ko.fu.dev@gmail.com>
2022-05-23 15:49:02 +0900

    Add

--- btrfs.sh
+++ btrfs.sh
@@ -2,7 +2,9 @@
 mkdir data
 dd if=/dev/zero of=hoge.img bs=1G count=2
 mkfs.btrfs hoge.img
-sudo mount -t btrfs -o loop,nodatacow,discard=async hoge.img data/
+# discard=async is not supported on Ubuntu?
+sudo mount -t btrfs -o loop,nodatacow hoge.img data/
+#sudo mount -t btrfs -o loop,nodatacow,discard=async hoge.img data/
 
 cd data
 btrfs subvolume snapshot . s0
